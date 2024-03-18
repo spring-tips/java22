da73b56b78159184af05455721bce1296ab0166d
Josh Long <josh@joshlong.com>
2024-03-18 19:41:38 -0400

    jre

---
+++ setup_jre.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "hello, world!"
+
+TGZ=https://github.com/graalvm/oracle-graalvm-ea-builds/releases/download/jdk-22.0.0-ea.07/graalvm-jdk-22.0.0-ea.07_linux-x64_bin.tar.gz
+curl $TGZ
+
+mkdir -p $HOME/bin/
