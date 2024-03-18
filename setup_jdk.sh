4b4995d03b78f073b0c72ced59ac3406813b2afa
Josh Long <josh@joshlong.com>
2024-03-18 19:55:15 -0400

    up

--- setup_jdk.sh
+++ setup_jdk.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-echo "hello, world!"
+
 
 TGZ=https://github.com/graalvm/oracle-graalvm-ea-builds/releases/download/jdk-22.0.0-ea.07/graalvm-jdk-22.0.0-ea.07_linux-x64_bin.tar.gz
 curl $TGZ
 
-cd $HOME
 
 pwd
 
