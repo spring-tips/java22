5f79650a50511972828ae482a42e66762e552d9e
Josh Long <josh@joshlong.com>
2024-03-18 21:47:51 -0400

    up

--- jdk.sh
+++ jdk.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-apt install wget
 set -euo pipefail
-mkdir -p $JAVA_HOME
 LOCAL_TGZ=jdk.tgz
 REMOTE_TGZ=https://github.com/graalvm/oracle-graalvm-ea-builds/releases/download/jdk-22.0.0-ea.07/graalvm-jdk-22.0.0-ea.07_linux-x64_bin.tar.gz
-wget -O $LOCAL_TGZ $REMOTE_TGZ
-#curl -o $LOCAL_TGZ $REMOTE_TGZ
-ls -la $JAVA_HOME || echo "could not stat $JAVA_HOME"
+#export JAVA_HOME="$HOME/Desktop/jdk_home"
+apt install wget || echo "could not install wget"
+
+mkdir -p "$JAVA_HOME"
+rm -rf "$JAVA_HOME"
+ls -la $LOCAL_TGZ || wget -O $LOCAL_TGZ $REMOTE_TGZ
+ls -la "$JAVA_HOME" || echo "could not stat $JAVA_HOME"
 ls -la $LOCAL_TGZ || echo "could not find local downloaded, $LOCAL_TGZ"
-du -hs $LOCAL_TGZ
 tar -zxf "$LOCAL_TGZ" -C .
-ls -la $JAVA_HOME
+ls -la "$JAVA_HOME"
 mv "graalvm-jdk-22+36.1" "$JAVA_HOME"
 
 java --version
