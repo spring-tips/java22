671c71186ef9b6d5855a4f24a81a0a066b8a792f
Josh Long <josh@joshlong.com>
2024-03-18 20:47:00 -0400

    up

--- jdk.sh
+++ jdk.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 mkdir -p $JAVA_HOME
 LOCAL_TGZ=jdk.tgz
 REMOTE_TGZ=https://github.com/graalvm/oracle-graalvm-ea-builds/releases/download/jdk-22.0.0-ea.07/graalvm-jdk-22.0.0-ea.07_linux-x64_bin.tar.gz
-curl $REMOTE_TGZ -o  $LOCAL_TGZ
+curl -o $LOCAL_TGZ $REMOTE_TGZ
 ls -la $JAVA_HOME || echo "could not stat $JAVA_HOME"
 ls -la $LOCAL_TGZ || echo "could not find local downloaded, $LOCAL_TGZ"
 du -hs $LOCAL_TGZ
