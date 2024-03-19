dba1c40b87d0d882212974629fc161b1ffae2325
Josh Long <josh@joshlong.com>
2024-03-19 16:02:28 -0400

    up

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,39 @@
+package com.example.demo;
 
-import com.example.demo.ApplicationConfiguration;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-void main(String[] args) {
-	SpringApplication.run(ApplicationConfiguration.class, args);
+import java.lang.foreign.Linker;
+import java.lang.foreign.SymbolLookup;
+import java.util.Map;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	SymbolLookup symbolLookup() {
+		var nativeLinker = Linker.nativeLinker();
+		var stdlibLookup = nativeLinker.defaultLookup();
+		var loaderLookup = SymbolLookup.loaderLookup();
+		return name -> loaderLookup.find(name).or(() -> stdlibLookup.find(name));
+	}
+
+	@Bean
+	ApplicationRunner demo(Map<String, LanguageDemonstrationRunner> demos) {
+		return _ -> demos.forEach((_, demo) -> {
+			try {
+				demo.run();
+			} //
+			catch (Throwable e) {
+				throw new RuntimeException(e);
+			}
+		});
+	}
+
 }
