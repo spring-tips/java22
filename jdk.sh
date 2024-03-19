92969b4dcfed4dd835cd7fdbefc28f33b4e945b4
Josh Long <josh@joshlong.com>
2024-03-18 20:31:20 -0400

    up

--- jdk.sh
+++ jdk.sh
@@ -2,6 +2,6 @@
 
 LOCAL_TGZ=jdk.tgz
 REMOTE_TGZ=https://github.com/graalvm/oracle-graalvm-ea-builds/releases/download/jdk-22.0.0-ea.07/graalvm-jdk-22.0.0-ea.07_linux-x64_bin.tar.gz
-curl $TGZ > $LOCAL_TGZ
+curl $REMOTE_TGZ > $LOCAL_TGZ
 tar -zxpf $LOCAL_TGZ
 ls -la $LOCAL_TGZ
