4679445feef97930f9c27a143f111fb67642bf90
Josh Long <josh@joshlong.com>
2024-03-18 22:38:49 -0400

    up

--- src/main/java/com/example/demo/DemoFeature.java
+++ src/main/java/com/example/demo/DemoFeature.java
@@ -2,11 +2,8 @@ package com.example.demo;
 
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeForeignAccess;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.foreign.FunctionDescriptor;
-import java.lang.foreign.SymbolLookup;
 
 import static java.lang.foreign.ValueLayout.*;
 
@@ -18,8 +15,9 @@ public class DemoFeature implements Feature {
     @Override
     public void duringSetup(DuringSetupAccess access) {
         System.out.println("registering the PRINTF function for downcalls during compilation.");
-        RuntimeForeignAccess.registerForDowncall(
-                FunctionDescriptor.of(JAVA_INT, JAVA_LONG,JAVA_LONG)
-        );
+        RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.of(JAVA_INT, JAVA_LONG, JAVA_LONG));
+        RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.of(ADDRESS, JAVA_LONG, JAVA_LONG));
+        RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.of(JAVA_INT_UNALIGNED,
+                JAVA_LONG, JAVA_LONG));
     }
 }
