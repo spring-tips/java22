b92875c6969cdf16c46a229d45f5b47809fce88f
Josh Long <josh@joshlong.com>
2024-03-18 21:52:23 -0400

    up

--- jdk.sh
+++ jdk.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 LOCAL_TGZ=jdk.tgz
 REMOTE_TGZ=https://github.com/graalvm/oracle-graalvm-ea-builds/releases/download/jdk-22.0.0-ea.07/graalvm-jdk-22.0.0-ea.07_linux-x64_bin.tar.gz
 apt install wget
+#wget $REMOTE_TGZ -O $LOCAL_TGZ
+
+wget -O $LOCAL_TGZ $REMOTE_TGZ
+
 mkdir -p "$JAVA_HOME"
 rm -rf "$JAVA_HOME"
 tar -zxf "$LOCAL_TGZ" -C .
