b49bbf1933d39d35af59a2433f4c7b5620c44d58
Josh Long <josh@joshlong.com>
2024-03-19 01:15:07 -0400

    up

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.stdio.stdio_h;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -107,8 +108,7 @@ class JExtractFfi implements LanguageDemonstrationRunner {
 
     @Override
     public void run() throws Throwable {
-
-        stdio_h.putchar( (int) 'c');
+        stdio_h.putchar((int) 'c');
     }
 
 }
