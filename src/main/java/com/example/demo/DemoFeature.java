b21e245433668325d915eb61b414b16a38449698
Josh Long <josh@joshlong.com>
2024-03-18 23:11:09 -0400

    up

--- src/main/java/com/example/demo/DemoFeature.java
+++ src/main/java/com/example/demo/DemoFeature.java
@@ -3,11 +3,7 @@ package com.example.demo;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeForeignAccess;
 
-import java.lang.foreign.FunctionDescriptor;
-import java.lang.foreign.Linker;
-
 import static com.example.demo.DemoApplication.PRINTF_FUNCTION_DESCRIPTOR;
-import static java.lang.foreign.ValueLayout.*;
 
 /**
  * registers the printf call for Graalvm native images
@@ -16,9 +12,6 @@ public class DemoFeature implements Feature {
 
     @Override
     public void duringSetup(DuringSetupAccess access) {
-        System.out.println("registering the PRINTF function for downcalls during compilation.");
         RuntimeForeignAccess.registerForDowncall(PRINTF_FUNCTION_DESCRIPTOR);
-        System.out.println("registered PRINTF functions");
-
     }
 }
