27d809d92c834a5baa4c543355636ae94d8e631a
Josh Long <josh@joshlong.com>
2024-03-18 20:29:04 -0400

    up

--- jdk.sh
+++ jdk.sh
@@ -2,16 +2,6 @@
 
 
 TGZ=https://github.com/graalvm/oracle-graalvm-ea-builds/releases/download/jdk-22.0.0-ea.07/graalvm-jdk-22.0.0-ea.07_linux-x64_bin.tar.gz
-curl $TGZ
-
-
-pwd
-
 curl $TGZ > jdk.tgz
-
 tar -zxpf $TGZ
-
 ls -la
-
-
-
