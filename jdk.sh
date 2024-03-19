cb6d55fc65f44db0efa3fa5cae8aaf64c891318f
Josh Long <josh@joshlong.com>
2024-03-18 21:50:12 -0400

    up

--- jdk.sh
+++ jdk.sh
@@ -8,11 +8,7 @@ apt install wget || echo "could not install wget"
 
 mkdir -p "$JAVA_HOME"
 rm -rf "$JAVA_HOME"
-ls -la $LOCAL_TGZ || wget -O $LOCAL_TGZ $REMOTE_TGZ
-ls -la "$JAVA_HOME" || echo "could not stat $JAVA_HOME"
-ls -la $LOCAL_TGZ || echo "could not find local downloaded, $LOCAL_TGZ"
 tar -zxf "$LOCAL_TGZ" -C .
-ls -la "$JAVA_HOME"
 mv "graalvm-jdk-22+36.1" "$JAVA_HOME"
 
 java --version
